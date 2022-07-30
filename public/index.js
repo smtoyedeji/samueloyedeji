c95e598904b83e24134bb3c242d96802b9754fe3
Samuel Oyedeji <32018370+sammyteeoye@users.noreply.github.com>
2022-07-30 20:17:07 +0100

    UI update

--- public/index.js
+++ public/index.js
@@ -18,6 +18,33 @@ $(window).scroll(function(){
 });
 
 
+//show elements on scroll
+const scrollElements = document.querySelectorAll(".scroll");
+
+//element in view
+const elementInView = (el, percentageScroll = 100) => {
+    const elementTop = el.getBoundingClientRect().top;
+    return (
+        elementTop <= ((window.innerHeight || document.documentElement.clientHeight) * (percentageScroll / 100))
+    );
+};
+
+//scroll function
+const displayScrollElement = (element) => {
+    element.classList.add("scrolled");
+};
+
+const handleScrollAnimation = () => {
+    scrollElements.forEach((el) => {
+        if (elementInView(el, 100)) {
+            displayScrollElement(el);
+        }
+    });
+};
+
+window.addEventListener("scroll", () => {
+    handleScrollAnimation();
+});
 
 
   
