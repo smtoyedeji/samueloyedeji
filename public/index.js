065cd763ceeb484fb33a505619dd15b485ef5b0a
Samuel Oyedeji <32018370+sammyteeoye@users.noreply.github.com>
2022-08-06 19:16:53 +0100

    Autotype feature added

--- public/index.js
+++ public/index.js
@@ -1,6 +1,8 @@
+//abiyaj@classvisa.com
+
 $(document).ready(function () {
     $(".nav-toggler").each(function(_, navToggler) {
-        var target = $(navToggler).data("target");
+        let target = $(navToggler).data("target");
         $(navToggler).on("click", function () {
             $(target).animate({
                 height:"toggle",
@@ -10,7 +12,7 @@ $(document).ready(function () {
 })
 
 $(window).scroll(function(){
-    var sticky = $('header'),
+    let sticky = $('header'),
         scroll = $(window).scrollTop();
   
     if (scroll >= 100) sticky.addClass('fixed').css({backgroundColor: '#4a5568'});
@@ -46,18 +48,52 @@ window.addEventListener("scroll", () => {
     handleScrollAnimation();
 });
 
+//Auto typing effect
+const stringArray = ["I have good problem solving skills", "I pay attention to details", "I'm a strong team player", "Excellent communication skills", "Resourceful and Personable", "Great multitasking skills"];
 
+let arrayIndex = 0;
+let data;
+const pause = 200;
+const addTime = 150;
+const removeTime = 25;
+let letterIndex = 0;
+let currentInterval
+
+const autotype = document.querySelector("#autotype");
+
+function textRotation() {
+  if(arrayIndex == stringArray.length) {
+    arrayIndex = 0;
+  };
   
+  data = stringArray[arrayIndex];
+  letterIndex = 0;
+  currentInterval = window.setInterval(addLetter, addTime);
+};
 
+function addLetter() {
+  autotype.innerHTML += data.charAt(letterIndex);
+  letterIndex += 1;
+  
+  if(letterIndex > data.length) {
+    window.clearInterval(currentInterval);
+    window.setTimeout(startRemove, pause);
+  };
+};
 
+function startRemove() {
+  currentInterval = window.setInterval(removeLetter, removeTime);
+}
 
+function removeLetter() {
+  let currentString = autotype.innerHTML;
+  autotype.innerHTML = currentString.slice(0, -1);
+ 
+  if(currentString.length < 1) {
+    window.clearInterval(currentInterval);
+    arrayIndex += 1;
+    textRotation();
+  };
+};
 
-
-
-
-
-
-
-
-
-
+window.onload = window.setTimeout(textRotation, 500);
